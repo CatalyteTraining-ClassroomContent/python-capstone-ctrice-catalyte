7ea304bbdb1684c47abfa90750bf6551cf0fcc94
Christian Trice <ctrice@catalyte.io>
2025-09-24 11:05:29 -0500

    Python Capstone

--- requirements.py
+++ requirements.py
@@ -1,15 +1,15 @@
 list_of_subs = [
     {
-        "quizName": "Math",
-        "quizModule": "Geometry",
+        "quizName": "Geometry",
+        "quizModule": "Math",
         "quizScore": 50,
         "studentId": 1,
         "studentName": "Jasmine",
         "submissionDate": "2025-07-08",
     },
     {
-        "quizName": "History",
-        "quizModule": "World History",
+        "quizName": "World history",
+        "quizModule": "History",
         "quizScore": 80,
         "studentId": 1,
         "studentName": "Jasmine",
@@ -25,7 +25,7 @@ list_of_subs = [
     },
     {
         "quizName": "Parameters",
-        "quizModule": "Geometry",
+        "quizModule": "Math",
         "quizScore": 95,
         "studentId": 3,
         "studentName": "Kim",
@@ -57,6 +57,12 @@ def filter_by_date(date, Submissions):
 
 
 def filter_by_student_id(studentId, submissions):
+    """
+    Filter through the students id and get list
+
+    Parameters:
+
+    """
     list_of_studentsIds = []
     for submission in submissions:
         if submission["studentId"] == studentId:
@@ -76,16 +82,9 @@ def get_students_name(submissions):
 
 
 def find_unsubmitted(date, students_name, submissions):
+    """ """
     completed_list = filter_by_date(date, submissions)
-    print(completed_list)
-    new_list = []
-    # i = 0
-    # for student in students_name:
-    #     if student != not_completed_list[i]["studentName"]:
-    #         # i += 1
-    #         new_list.append(student)
-
-    return [name for name in students_name if name not in completed_list]
+    return [student for student in students_name if student not in completed_list]
 
 
 def get_average_score(submissions):
@@ -107,47 +106,25 @@ def get_average_score(submissions):
     return round(total / l, 1)
 
 
-# Quiz Average by Module Feature (get_average_score_by_module)
-# 1. Given I have a list of submission objects, when I supply that list to the
-# get_average_score_by_module function, I am returned an object.
-# 2. Given that I have received an object from this feature, then there is one key for
-# every unique module name in the submission list, and the keys are the module
-# names.
-# 3. Given that I have a list of submission objects from only one module, when I use the
-# quiz average by module feature, then the resulting object contains only one key.
-# 4. When I have received an object from this feature, the value of each key should be
-# the average of quiz scores from that module.
-# Example:
-# {
-# "Statistics": 83.5,
-# "Algebra": 79.6,
-# "History": 80.1
-# }
-def get_list(module, submissions):
-    list = []
-    for submission in submissions:
-        if submission["quizModule"] == module:
-            list.append(submission)
-    return list
-
-
 def get_average_score_by_module(submissions):
     modules_averages = {}
+    counts = {}
+    scores = {}
 
     for submission in submissions:
-        for key, value in modules_averages.items():
-            if key == submission["quizModule"]:
-                value += submission["quizScore"]
-        if modules_averages[submission["quizModule"]] != submission["quizModule"]:
-            modules_averages[submission["quizModule"]] = submission["quizScore"]
+        module = submission["quizModule"]
+        score = submission["quizScore"]
+
+        if module not in scores:
+            scores[module] = 0
+            counts[module] = 0
+        scores[module] += score
+        counts[module] += 1
+
+    # for submission in submissions:
+    #     modules_averages[submission["quizModule"]] = submission["quizScore"]
 
     return modules_averages
 
 
-print(get_average_score_by_module(list_of_subs))
-# print(get_list("Geometry", list_of_subs))
-# print(get_students_name(list_of_subs))
-# print(find_unsubmitted("2025-07-08", ["Jasmine", "Kim"], list_of_subs))
-# # print(filter_by_date("2020-07-10", list_of_subs))
-# print(get_average_score(list_of_subs))
-# print(filter_by_student_id(2, list_of_subs))
+get_average_score_by_module(list_of_subs)
